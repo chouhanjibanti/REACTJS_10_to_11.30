9e8976a892e6d8c74a92cdc50d1f68cfb168fca0
Banti Chouhan <chouhanbanti224@gmail.com>
2025-05-08 11:25:27 +0530

    DAY_1

---
+++ DAY1/README.md
@@ -0,0 +1,64 @@
+REACTJS
+
+JS:- 
+let , const , var 
+
+JS ES6 :- 
+let , const
+var -> ES6 before
+
+var a = 10;
+var a = 30;
+
+let , const
+
+let a= 10;
+a = 30;
+clg(a)
+
+const a =30;
+a = 40;
+
+let a= 10;
+let a =40;
+=====================================
+
+arrow function :- 
+()=>{ }
+
+============================
+
+module :- import / export
+
+=====================
+
+hoisting :- 
+
+======================
+
+two type :- 
+inside the browser
+outside the browser
+
+=======================
+
+HOF and CBF
+
+======================
+
+IIFE :- imediate  invoke function  expression
+(dmnvn)()
+
+nested function :- 
+
+================================
+
+JS:- DOM       actual , recreate 
+
+REACTJS :- virtual DOM SPA    -> diffing 
+reconcilation 
+
+npx create reactapp appname -    home.js
+
+npm create vite@latest aapname    home.jsx
+
---
+++ DAY1/index.js
@@ -0,0 +1,152 @@
+// let a= 10;
+// let a =40;
+
+// const a = 10;
+// const a =40;
+
+// function create 
+// decla
+
+// function demo(){
+//     console.log("start")
+// }
+// demo()
+
+// express
+
+// let demo1 = function(){
+//     console.log("start")
+// }
+// demo1()
+
+
+// arrow function 
+// let demo2 = () =>{  console.log("end")}
+// demo2()
+
+// let sum = (a,b) =>{return a+b}
+// console.log(sum(10,20))
+
+
+// ===========================
+
+// demo()
+// function demo(){
+//     console.log("hoisting")
+// }
+
+
+// test()
+// let test = function(){
+//     console.log("hoisting1")
+// }
+
+// demo2()
+// let demo2 = () =>{  console.log("end")}
+
+// HOF : it is an function that takes another function as an argument or returns a function as an output
+
+// function demo(CBF){
+//    CBF()
+// }
+// function demo1(){
+//     console.log("CBF")
+// }
+
+
+// IIFE
+
+// (function test1(){
+//     console.log("end")
+// })()
+
+// nested function 
+
+// function demo(){
+//  console.log("1234")
+//     function demo1(){
+//         console.log("end")
+//     }
+//     demo1()
+// }
+// demo()
+
+
+// spread operator 
+// const body = req.body;
+// File.push(...body)
+
+// ==================================
+
+// === and ==
+
+// == -> value  5 == '5 '
+// === -> value and datatype
+
+// ==================================
+
+// object :- 
+
+// object is a collection of key value pairs
+
+// const person = {
+//     name: "kritanshu",
+//     id : 1,
+//     phone :6789876
+// }
+// console.log(person)
+// console.log(person.name)
+// console.log(person.id)
+
+// array method 
+// push
+// pop// shift 
+// unshift /// slice .. splice .. sort .. reverse .. map .. filter .. reduce .. find .. findIndex .. forEach .. some ..
+
+// datatype :
+// primitive and non primitive dt
+
+// primitive dt :- number , string , boolean , null , undefined , symbol
+// array , object , function
+
+// DOM :- 
+
+
+// event listener
+// event listener is a function that is called when an event occurs
+
+// onclick 
+// onMouseOver
+// onPressKey
+// onKeyDown
+
+// ==================================
+
+// promises: - 
+
+// resolve , reject , pending 
+
+// synchronous and asynchronous
+// setTimeout 5000 -> 5s
+// async and await 
+
+// =============================
+
+// switch
+
+// loops :- for , while , do while , for of , for in , for each , for of ,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
